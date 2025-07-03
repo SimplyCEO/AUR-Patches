38a874a3d0a30a0cb41f8497fdda76005f9e1e83
SimplyCEO <simplyceo.developer@gmail.com>
2025-07-03 01:45:21 -0300

    Use `.SRCINFO` file to get `PKGNAME` variable

--- makepatcher.sh
+++ makepatcher.sh
@@ -5,76 +5,7 @@
 
 # From: https://github.com/SimplyCEO/AUR-Patches
 
-# --> Retrieve the package name from array.
-PKGARRAY=""
-pkgarray()
-{
-  local L1COUNT=0
-  local STRING=""
-  local LSTART=$1
-  local FILE="$2"
-  local PKGKEY="$3"
-
-  while IFS= read -r LINE; do
-    if [ $L1COUNT -ge $LSTART ]; then
-      STRING="${STRING}${LINE}"
-      if [ "${LINE}" = ")" ]; then break; fi
-    fi
-    L1COUNT=$((L1COUNT + 1))
-  done < "${FILE}"
-
-  STRING=${STRING#${PKGKEY}=(}
-  STRING=${STRING%)*}
-
-  local ARRAY=($STRING)
-
-  PKGARRAY="${ARRAY[0]//\'/}"
-}
-
-# --> Retrieve the package name based on key.
 PKGNAME=""
-get_package_name()
-{
-  local FILETMP=$(mktemp)
-  local PACKAGE=""
-
-  # --> Read package until key value.
-  local LCOUNT=0
-  local FILE="$1"
-  local KEY="$2"
-
-  if [ -f "${FILE}" ]; then
-    while IFS= read -r LINE; do
-      if printf "${LINE}" | grep -q "^${KEY}="; then
-        PACKAGE="${LINE#${KEY}=}"
-        if [ "${PACKAGE}" = "(" ]; then
-          pkgarray $LCOUNT "${FILE}" "${KEY}"
-          PACKAGE="${PKGARRAY}"
-        fi
-        printf "PATCHER_RETURN=${PACKAGE}\n" >> "${FILETMP}"
-        break
-      fi
-
-      LCOUNT=$((LCOUNT + 1))
-      if [ $LCOUNT -gt 20 ]; then break; fi
-
-      printf "${LINE}\n" >> "${FILETMP}"
-    done < "${FILE}"
-  fi
-
-  # --> Source the key and retrieve the value
-  . "${FILETMP}"
-  PACKAGE="${PATCHER_RETURN}"
-  rm "${FILETMP}"
-
-  # --> Return error if no package was found.
-  if [ -z "${PACKAGE}" ]; then return 1; fi
-
-  # --> Assign to variable.
-  PKGNAME="${PACKAGE}"
-
-  return 0
-}
 
 # --> Retrieve package file from repository, if exist.
 fetch_patch()
@@ -92,19 +23,9 @@ fetch_patch()
 }
 
 # --> If PKGBUILD is already modified then skip.
-if ! git diff --quiet PKGBUILD; then
-  printf "\033[1;34m::\033[0m \033[1mPKGBUILD has already been patched before. Continuning...\033[0m\n" >&2
-else
-  # --> Fetch existing patch file for PKGBUILD.
-  get_package_name "PKGBUILD" "_pkgbase"
-  if [ $? -ne 0 ]; then
-    get_package_name "PKGBUILD" "_pkgname"
-    if [ $? -ne 0 ]; then
-      get_package_name "PKGBUILD" "pkgname"
-      if [ $? -ne 0 ]; then get_package_name "PKGBUILD" "pkgbase"; fi
-    fi
-  fi
-  if [ $? -eq 0 ]; then fetch_patch "PKGBUILD"; fi
+if git diff --quiet PKGBUILD; then
+  PKGNAME=$(cat .SRCINFO | grep "pkgname" | head -n 1 | cut -d " " -f 3)
+  if [ ! -z ${PKGNAME} ]; then fetch_patch "PKGBUILD"; fi
 
   # --> Patch the PKGBUILD file if there is one for it.
   if [ -f PKGBUILD.patch ]; then
@@ -115,5 +36,7 @@ else
       makepkg -g >> PKGBUILD
     fi
   fi
+else
+  printf "\033[1;34m::\033[0m \033[1mPKGBUILD has already been patched before. Continuning...\033[0m\n" >&2
 fi
 
